f9734346315595e398b51e0ff306eba581e12882
MeharabRaj13 <125471416+MeharabRaj13@users.noreply.github.com>
2023-02-15 23:21:47 +0000

    Create meharab-login.py

---
+++ meharab-login.py
@@ -0,0 +1,17 @@
+print("What is your name?")
+name = input()
+print("Please enter your password")
+pw = input()
+if name == "Meharab" and pw ==  "riceroll":
+    print("Welcome you idiot")
+while name != "Meharab" or pw != "riceroll":
+    print("try again you imposter")
+    print("What is your name?")
+    name = input()
+    print("Please enter your password")
+    pw = input()
+    if name == "Meharab" and pw ==  "riceroll":
+        print("Welcome you idiot")
+
+
+# I had been helped by ahmed on this small project 
