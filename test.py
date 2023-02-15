5a48227c75192e9e18f201ac3574df45dd0a6bbf
SyedHassanHaider <quantumbeavergaming78902@gmail.com>
2023-02-15 16:43:15 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ name = input()
 print("Enter your password")
 pw = input()
 if name == "Hassan" and pw == "Lovemaaz123":
-    then print("Welcome to your account syed")
+    print("Welcome to your account syed")
 while name != "Hassan" or pw != "Lovemaaz123":
     print("Incorrect username and/or password, try again")
     print("Enter your username")
@@ -13,3 +13,6 @@ while name != "Hassan" or pw != "Lovemaaz123":
     if name == "Hassan" and pw == "Lovemaaz123":
         print("Welcome to your account syed")
 print("Your name is,", name,"and your password is,",pw)
+# a colon represents then 
+#use and/while statements
+#don't forget inverted commas
